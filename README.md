fea50ef85e01bb95766f8014388379220dc75927
Khikmatillo <mr.khikmatillo@mail.ru>
2018-03-05 15:09:05 +0500

    assignment1

---
+++ README.md
@@ -0,0 +1,27 @@
+#Phliker App
+
+###Phliker is very simple Photo Gallery app that retrieves images from public API of well-known image service [Flickr](https://www.flickr.com/)
+
+##Services:
+1. Retrieve images from [Flickr API](https://www.flickr.com/services/api/) according to given tag
+1. Load 40 images per pages
+1. Pagination according to number of responses
+1. Retrieve description for selected image
+    * Image title
+    * Location information
+        * Country
+        * Region
+        * Place
+
+
+
+
+**Note:** This is a practice app to get experience with:
+* HTML5
+* CSS3
+* JavaScript
+* Jquery
+* Building single page apps
+* Bootstrap
+* Integrating public web servises
+* Exchanging JSON data 
--- js/app.js
+++ js/app.js
@@ -1,53 +1,61 @@
 $(document).ready(function (){
+
+    //initialize global variables starts ------------------
     var MAX_PAGES = 6;
     var KEY = "a8a36cee073bf89c60264ceb317ee800";
     var PHOTOS;
     var INDEX = 0;
+    //initialize global variables ends ------------------
+
+    //Functions to use more than once initializations starts -------------------------------------------
+
+    //Function loads images for chosen page
     function loadPage(page, tags) {
         var showData = $("#search-result");
         console.info(tags);
+        //custom link for chosen page
         var link = "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=" + KEY +
             "&tags=" + tags + "&per_page=40&page=" + page + "&format=json&nojsoncallback=1";
 
-        // console.info("link: " + link);
-
-        $.get(link, function (data, status) {
+        $.get(link, function (data) {
             MAX_PAGES = data.photos.pages;
-            // console.info("Maxpages in func " + max_pages);
-            // console.info("status " + status);
             console.info(data);
             PHOTOS = data.photos.photo;
             $("#text-search1").val(tags);
             showData.empty();
             var len = PHOTOS.length;
             var list = "";
+            //load images in 8 rows and 5 columns format
             for(var i = 0; i < len/5; i++){
                 var content = "";
+                //load retrieved photos into a variable
                 for(var j = 0; j < 5; j++){
                     var index = 5 * i + j;
                     var farm_id = PHOTOS[index].farm;
                     var server_id = PHOTOS[index].server;
                     var id = PHOTOS[index].id;
                     var secret = PHOTOS[index].secret;
+                    //custom link for each image
                     var img_link = "https://farm" + farm_id + ".staticflickr.com/" + server_id + "/" + id + "_" + secret + ".jpg";
                     content += '<img src="' + img_link + '" class="img-show-info" id=' + id + ' style="width: 100%"/>';
                 }
                 list += '<div class="column">' + content + '</div>';
             }
-
+            //display retrieved images
             $("#div-loading").hide();
             showData.append(list);
 
+            //show no image if result is empty
             if(len === 0){
                 var content1 = '<p>No data for tag ' + tags + '</p>';
                 showData.append(content1);
             }
         });
-
+        //display loading message until function $.get callback
         $("#div-loading").text('Loading images from https://www.flickr.com/');
     }
 
-
+    //Function retrieves JSON data for given tag
     function searchAction(type) {
         var tags;
         if(type === 1){
@@ -55,11 +63,12 @@ $(document).ready(function (){
         }else{
             tags = $("#text-search1").val();
         }
-
+        //do operation only if tag is provided
         if(tags !== ""){
             $("#div-search").hide();
             loadPage(1, tags);
             console.info(MAX_PAGES);
+            //initialize pagination
             $("#boot-pag").bootpag({
                 total: MAX_PAGES,
                 page: 1,
@@ -78,12 +87,15 @@ $(document).ready(function (){
 
     }
 
+    //Function loads current, next or previous photo to the modal
     function loadAnotherImage(index) {
         var farm_id = PHOTOS[index].farm;
         var server_id = PHOTOS[index].server;
         var id = PHOTOS[index].id;
         var secret = PHOTOS[index].secret;
+        //link to get original image
         var img_link = "https://farm" + farm_id + ".staticflickr.com/" + server_id + "/" + id + "_" + secret + ".jpg";
+        //link to get image description
         var link_info = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key="+ KEY +
             "&photo_id=" + id + "&format=json&nojsoncallback=1";
 
@@ -91,6 +103,7 @@ $(document).ready(function (){
             var img_title = data.photo.title._content;
             var img_location = data.photo.location;
             var img_country, img_place, img_region;
+            //check title and location information is provided
             if(img_title === undefined){
                 img_title = "No title";
             }
@@ -103,29 +116,30 @@ $(document).ready(function (){
                 img_place = img_location.locality._content;
                 img_region = img_location.region._content;
             }
-
+            //change content of modal and display it
             $("#modal-img-title").text(img_title);
             $("#modal-img-source").html("<img src='" + img_link + "'>");
             $("#modal-img-desc").html("<p>Country: " + img_country + "</p>" +
                 "<p>Region: " + img_region + "</p>" +
                 "<p>Place: " + img_place + "</p>");
-            // $("#modal-img-info").modal("show");
+            $("#modal-img-info").modal("show");
         });
 
     }
+    //Functions to use more than once initializations ends -------------------------------------------
 
+
+    //click events initialization starts -------------------------------------
     $("#btn-search").click(function(){
         searchAction(1);
     });
+
     $("#btn-search1").click(function(){
         MAX_PAGES = 6;
         searchAction(2);
     });
 
     $(document).on('click','.img-show-info',function(){
-        var link_info = "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key="+ KEY +
-            "&photo_id=" + this.id + "&format=json&nojsoncallback=1";
-        var link_img = this.src;
         var len = PHOTOS.length;
         for(var i = 0; i < len; i++){
             if(this.id === PHOTOS[i].id){
@@ -133,42 +147,12 @@ $(document).ready(function (){
             }
         }
         console.info("index: " + INDEX);
-
-        $.get(link_info, function (data) {
-            console.info(data);
-
-            var img_title = data.photo.title._content;
-            var img_location = data.photo.location;
-            var img_country, img_place, img_region;
-
-            if(img_title === undefined){
-                img_title = "No title";
-            }
-            if(img_location === undefined){
-                img_country = "No country info";
-                img_place = "No place info";
-                img_region = "No region info";
-            }else{
-                img_country = img_location.country._content;
-                img_place = img_location.locality._content;
-                img_region = img_location.region._content;
-            }
-
-            console.info("Title: " + img_title + ";\nCountry: " + img_country + ";\nPlace: " + img_place + ";\nRegion: " + img_region);
-            console.info("\nSRC: " + link_img);
-
-
-            $("#modal-img-title").text(img_title);
-            $("#modal-img-source").html("<img src='" + link_img + "'>");
-            $("#modal-img-desc").html("<p>Country: " + img_country + "</p>" +
-                                      "<p>Region: " + img_region + "</p>" +
-                                      "<p>Place: " + img_place + "</p>");
-            $("#modal-img-info").modal("show");
-        });
-
+        loadAnotherImage(INDEX);
     });
 
     $("#modal-img-next").click(function () {
+        //simulate next image as row
+        //in reality photos are located by column
         if(INDEX !== 39){
             if(INDEX > 34){
                 INDEX = (INDEX + 1) % 35;
@@ -180,6 +164,8 @@ $(document).ready(function (){
     });
 
     $("#modal-img-prev").click(function () {
+        //simulate prev image as row
+        //in reality photos are located by column
         if(INDEX !== 0){
             if(INDEX < 5){
                 INDEX = INDEX - 1 + 35;
@@ -189,6 +175,7 @@ $(document).ready(function (){
         }
         loadAnotherImage(INDEX);
     });
+    //click events initialization ends -------------------------------------
 });
 
 /*
